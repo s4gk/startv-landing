1a0070c4c91454d6d21a8beefa96cb50fc22819d
s4gk <s4gk.23@gmail.com>
2025-12-16 12:54:41 -0500

    fix: Add Profiles Testimonials

--- src/lib/PricingData.ts
+++ src/lib/PricingData.ts
@@ -1,23 +1,38 @@
 export const PricingData = [
   {
-    name: 'Plan Básico',
-    price: '100.000',
-    features: ['Velocidad de hasta 50 Mbps', 'Ideal para 1-2 dispositivos conectadosTV Esencial', 'Navegación web, redes sociales y streaming en calidad estándar'],
+    name: "Plan Básico",
+    price: "55.000",
+    features: [
+      "TV + Internet de 50 Mg",
+      "Ideal para 1-2 dispositivos conectadosTV Esencial",
+      "Navegación web, redes sociales y streaming en calidad estándar",
+    ],
     isPopular: false,
-    cta: 'Lo Quiero',
+    cta: "Lo Quiero",
   },
   {
-    name: 'Plan Premium',
-    price: '150.000',
-    features: ['Velocidad de hasta 500 Mbps', 'Perfecto para hogares inteligentes y gamers', 'Navegación web, redes sociales y streaming en calidad estándar', 'Gaming online sin lag ni caídas de conexión', 'Subidas y descargas ultrarrápidas (ideal para trabajar en la nube)'],
+    name: "Plan Premium",
+    price: "80.000",
+    features: [
+      "TV + Internet de 500 Mg",
+      "Perfecto para hogares inteligentes y gamers",
+      "Navegación web, redes sociales y streaming en calidad estándar",
+      "Gaming online sin lag ni caídas de conexión",
+      "Subidas y descargas ultrarrápidas (ideal para trabajar en la nube)",
+    ],
     isPopular: true,
-    cta: '¡El Más Vendido!',
+    cta: "¡El Más Vendido!",
   },
   {
-    name: 'Plan Standar',
-    price: '100.000',
-    features: ['Velocidad de hasta 150 Mbps', 'Soporta hasta 5 dispositivos conectados simultáneamente', 'Streaming en HD y videollamadas sin interrupciones', 'Subidas y descargas rápidas para trabajo remoto o clases online'],
+    name: "Plan Standar",
+    price: "70.000",
+    features: [
+      "TV + Internet de 300 Mg",
+      "Soporta hasta 5 dispositivos conectados simultáneamente",
+      "Streaming en HD y videollamadas sin interrupciones",
+      "Subidas y descargas rápidas para trabajo remoto o clases online",
+    ],
     isPopular: false,
-    cta: 'Lo Quiero',
-  }
+    cta: "Lo Quiero",
+  },
 ];
--- src/lib/TestimonialData.ts
+++ src/lib/TestimonialData.ts
@@ -3,38 +3,38 @@ import { ITestimonialData } from "../types";
 export const TestimonialData: ITestimonialData[] = [
   {
     id: 1,
-    image: "/images/profile.webp",
-    name: "Nombre De Usuario",
-    occupation: "Ocupación De Usuario",
+    image: "/images/profile_1.webp",
+    name: "Carlos Andrés Moreno",
+    occupation: "Administrador de Empresas",
     comment:
-      "La velocidad y estabilidad del servicio superaron mis expectativas. Ahora disfruto de mis series sin interrupciones.",
+      "Lo que más valoro es que la velocidad se mantiene incluso en la noche. La instalación fue rápida y clara desde el primer día.",
     rating: 4,
   },
   {
     id: 2,
-    image: "/images/profile.webp",
-    name: "Nombre De Usuario",
-    occupation: "Ocupación De Usuario",
+    image: "/images/profile_2.webp",
+    name: "Santiago Garcia",
+    occupation: "Desarrollador Software",
     comment:
-      "La velocidad y estabilidad del servicio superaron mis expectativas. Ahora disfruto de mis series sin interrupciones.",
-    rating: 4,
+      "Trabajo desde casa y necesitaba una conexión estable todo el día. Desde que tengo este servicio no he tenido caídas ni problemas de velocidad.",
+    rating: 5,
   },
   {
     id: 3,
-    image: "/images/profile.webp",
-    name: "Nombre De Usuario",
-    occupation: "Ocupación De Usuario",
+    image: "/images/profile_3.webp",
+    name: "Laura Sofía Ríos",
+    occupation: "Diseñadora gráfica freelance",
     comment:
-      "La velocidad y estabilidad del servicio superaron mis expectativas. Ahora disfruto de mis series sin interrupciones.",
+      "Antes tenía muchos cortes. Con este proveedor la conexión es mucho más estable y puedo trabajar sin interrupciones.",
     rating: 4,
   },
   {
     id: 4,
-    image: "/images/profile.webp",
-    name: "Nombre De Usuario",
-    occupation: "Ocupación De Usuario",
+    image: "/images/profile_4.webp",
+    name: "Javier Eduardo Peña",
+    occupation: "Estudiante universitario",
     comment:
-      "La velocidad y estabilidad del servicio superaron mis expectativas. Ahora disfruto de mis series sin interrupciones.",
+      "El internet es rápido y confiable. No he tenido problemas con videollamadas ni juegos en línea.",
     rating: 4,
   },
 ];
